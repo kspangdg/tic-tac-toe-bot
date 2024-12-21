9432010d021922298858820094812ca5337f6811
kspangdg <kspang@durkangroup.com>
2024-12-21 14:25:53 -0500

    weights update

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,13 +11,15 @@
 
 
 class NeuralNetwork {
-    constructor() {
+    constructor(weights_data, biases_data) {
         this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         this.layers = {
             input:{count: 27, cells: []},  // [1,0,0] = 0, [0,1,0] = 1, [0,0,1] = 2
             hidden:{count: 11, cells: []},
             output:{count: 9, cells: []}
         }
+        this.weights_data = weights_data;
+        this.biases_data = biases_data;
         this.calculating = false;
         this.game_started = false;
         this.game_over = false;
@@ -35,28 +37,8 @@ class NeuralNetwork {
     random(min, max) {
         return (Math.random() * (max - min + 1) + min).toFixed(2);
     }
-
-    print_data() {
-        const weights_div = document.getElementById('weights');
-        let html = 'weights: {<br>';
-        for (const property in this.layers) {
-            for (let i = 0; i < this.layers[property].count; i++) {
-                html += property + ' ' + i + ': [';
-                for (let j = 0; j < this.layers[property].cells[i].weights.length; j++) {
-                    html += this.layers[property].cells[i].weights[j] + ', ';
-                }
-                html += ']<br>';
-            }
-        }
-        html += '}<br>';
-        html += 'bias: {<br>';
-        for (const property in this.layers) {
-            for (let i = 0; i < this.layers[property].count; i++) {
-                html += property + ' ' + i + ': ' + this.layers[property].cells[i].bias + '<br>';
-            }
-        }
-        html += '}';
-        weights_div.innerHTML = html;
+    color_opacity(value) {
+       return Math.max(0, value);
     }
 
     check_outcome() {
@@ -79,7 +61,7 @@ class NeuralNetwork {
             }
         }
         // check for draw
-        if (!this.board.includes(0)) {
+        if (!this.board.includes(0) && !this.game_over) {
             this.game_over = true;
             this.outcome = 0;
             this.message.innerHTML = 'Draw!';
@@ -87,67 +69,98 @@ class NeuralNetwork {
         this.calculating = false;
     }
 
-    print(property) {
-        let x = 0;
-        let y = 0;
-        if (property === 'input') {
-            x = 10;
-            y = 10;
-        } else if (property === 'hidden') {
-            x = 400;
-            y = 10 * 17;
-        } else if (property === 'output') {
-            x = 800;
-            y = 10 * 19;
+    print_data() {
+        for (const property in this.layers) {
+            let html = '';
+            for (let i = 0; i < this.layers[property].count; i++) {
+                html += property + '_' + i + ': {v: ' + this.layers[property].cells[i].value.toFixed(2);
+                if (property !== 'output') {
+                    html += ', w: [';
+                    for (let j = 0; j < this.layers[property].cells[i].weights.length; j++) {
+                        html += this.layers[property].cells[i].weights[j] + ', ';
+                    }
+                    html += '], b: ' + this.layers[property].cells[i].bias;
+                }
+                html += '}<br>';
+            }
+            if (property === 'input') {
+                document.getElementById('inputs').innerHTML = html;
+            } else if (property === 'hidden') {
+                document.getElementById('hidden').innerHTML = html;
+            } else if (property === 'output') {
+                document.getElementById('outputs').innerHTML = html;
+            }
+                
         }
-        // Draw lines
-        if (!this.game_started) {
+    }
+    
+    print() {
+        this.ctx.clearRect(0, 0, 860, 540);
+        for (const property in this.layers) {
+            let x = 0;
+            let y = 0;
+            if (property === 'input') {
+                x = 10;
+                y = 10;
+            } else if (property === 'hidden') {
+                x = 400;
+                y = 10 * 17;
+            } else if (property === 'output') {
+                x = 800;
+                y = 10 * 19;
+            }
+            // Draw lines
             if (property === 'input') {
                 for (let i = 0; i < this.layers[property].count; i++) {
                     for (let j = 0; j < this.layers['hidden'].count; j++) {
-                        const weight = Math.max(0,this.layers[property].cells[i].weights[j]);
+                        const weight = this.color_opacity(this.layers[property].cells[i].weights[j]);
+                        // clear previous line
                         this.ctx.beginPath();
                         this.ctx.moveTo(10, y + (i * 20));
                         this.ctx.lineTo(400, y * 17 + (j * 20));
-                        this.ctx.strokeStyle = 'rgba(24, 231, 207, ' + weight + ')';
+                        console.log(weight);
+                        
+                        this.ctx.strokeStyle = (this.game_started) ? 'rgba(24, 231, 207, ' + weight + ')' : 'rgba(24, 231, 207, 0.1)';
                         this.ctx.stroke();
                     }
                 }
             } else if (property === 'hidden') {
                 for (let i = 0; i < this.layers[property].count; i++) {
                     for (let j = 0; j < this.layers['output'].count; j++) {
-                        const weight = Math.max(0,this.layers[property].cells[i].weights[j]);
+                        const weight = this.color_opacity(this.layers[property].cells[i].weights[j]);
                         this.ctx.beginPath();
                         this.ctx.moveTo(400, y + (i * 20));
                         this.ctx.lineTo(800, y + 20 + (j * 20));
-                        this.ctx.strokeStyle = 'rgba(24, 231, 207, ' + weight + ')';
+                        this.ctx.strokeStyle = (this.game_started) ? 'rgba(24, 231, 207, ' + weight + ')' : 'rgba(24, 231, 207, 0.1)';
                         this.ctx.stroke();
                     }
                 }
             }
-        }
-        for (let i = 0; i < this.layers[property].count; i++) {
-            let value = this.layers[property].cells[i].value;
-            let hsl_inverse = 50 * value;
-            
-            this.ctx.beginPath();
-            this.ctx.arc(x, y, 10, 0, 2 * Math.PI);
-            // increase stroke width
-            this.ctx.strokeStyle = '#18E7CF';
-            this.ctx.stroke();
-            this.ctx.fillStyle = 'hsl(173, 81%, ' + hsl_inverse + '%)';
-            this.ctx.fill();
-            // Draw text
-            // this.ctx.fillStyle = 'black';
-            // this.ctx.font = '10px Arial';
-            // this.ctx.fillText(value, x - 2.5, y + 2.5);
-            y += 20;
+            for (let i = 0; i < this.layers[property].count; i++) {
+                let value = this.layers[property].cells[i].value;
+                let hsl_inverse = 50 * value;
+                
+                this.ctx.beginPath();
+                this.ctx.arc(x, y, 10, 0, 2 * Math.PI);
+                // increase stroke width
+                this.ctx.strokeStyle = '#18E7CF';
+                this.ctx.stroke();
+                this.ctx.fillStyle = 'hsl(173, 81%, ' + hsl_inverse + '%)';
+                this.ctx.fill();
+                // Draw text
+                // this.ctx.fillStyle = 'black';
+                // this.ctx.font = '10px Arial';
+                // this.ctx.fillText(value, x - 2.5, y + 2.5);
+                y += 20;
+            }
         }
     }
 
     predict() {
         // Input layer
         for (let i = 0; i < this.board.length; i++) {
+            console.log('board: ' + i);
+            
             if (this.board[i] === 0) {
                 this.layers['input'].cells[i * 3].value = 1;
                 this.layers['input'].cells[i * 3 + 1].value = 0;
@@ -161,35 +174,29 @@ class NeuralNetwork {
                 this.layers['input'].cells[i * 3 + 1].value = 0;
                 this.layers['input'].cells[i * 3 + 2].value = 1;
             }
-            this.print('input');
         }
+        
         // Hidden layer
         for (let i = 0; i < this.layers['hidden'].count; i++) {
             let sum = 0;
             for (let j = 0; j < this.layers['input'].count; j++) {
-                sum += this.layers['input'].cells[j].value * Math.round(this.layers['input'].cells[j].weights[i]);
+                sum += this.layers['input'].cells[j].value * this.layers['input'].cells[j].weights[i];
             }
             sum += this.layers['hidden'].cells[i].bias;
-            this.layers['hidden'].cells[i].value = this.sigmoid(sum);
-            setTimeout(() => {
-                this.print('hidden');
-            } , 100);
+            this.layers['hidden'].cells[i].value = sum;
         }
         // Output layer
         for (let i = 0; i < this.layers['output'].count; i++) {
             let sum = 0;
             for (let j = 0; j < this.layers['hidden'].count; j++) {
-                sum += this.layers['hidden'].cells[j].value * Math.round(this.layers['hidden'].cells[j].weights[i]);
+                sum += this.layers['hidden'].cells[j].value * this.layers['hidden'].cells[j].weights[i];
             }
             sum += this.layers['output'].cells[i].bias;
             this.layers['output'].cells[i].value = this.sigmoid(sum);
-            setTimeout(() => {
-                this.print('output');
-            } , 200);
         }
-        setTimeout(() => {
-            this.computer_move();
-        }, 300);
+        //this.print();
+        this.print_data();
+        this.computer_move();
     }
 
     computer_move() {
@@ -203,9 +210,10 @@ class NeuralNetwork {
         }
         console.log('Computer move: ' + max + ' at index: ' + index);
         this.board[index] = 2;
-        this.grid_cells[index].innerHTML = 2;
+        this.grid_cells[index].innerHTML = 'o';
         document.getElementById('compute').innerHTML = index + 1;
         this.check_outcome();
+        this.print();
     }
 
     // User input evet listener
@@ -218,7 +226,7 @@ class NeuralNetwork {
                 self.game_started = true;
                 self.calculating = true;
                 self.board[i] = 1; // update board
-                self.grid_cells[i].innerHTML = 1; // update UI
+                self.grid_cells[i].innerHTML = 'x'; // update UI
                 self.check_outcome();
                 if (self.game_over) return;
                 self.predict();
@@ -252,22 +260,16 @@ class NeuralNetwork {
                 });
                 // Create weights
                 if (property === 'input') {
-                    for (let j = 0; j < this.layers['hidden'].count; j++) {
-                        this.layers[property].cells[i].weights.push(this.random(-1, 1));
-                    }
+                    this.layers[property].cells[i].weights = global_weights['i' + i];
+                    this.layers[property].cells[i].bias = global_biases['i' + i];
                 } else if (property === 'hidden') {
-                    for (let j = 0; j < this.layers['output'].count; j++) {
-                        this.layers[property].cells[i].weights.push(this.random(-1, 1));
-                    }
+                    this.layers[property].cells[i].weights = global_weights['h' + i];
+                    this.layers[property].cells[i].bias = global_biases['h' + i];
                 }
-                // Create bias
-                this.layers[property].cells[i].bias = Math.round(this.random(-20, 1));
             }
         }
         console.log(this.layers);
-        this.print('input');
-        this.print('hidden');
-        this.print('output');
+        this.print();
         this.user_input();
         this.print_data();
     }
@@ -275,5 +277,5 @@ class NeuralNetwork {
 }
 
 // Init Neural Network
-const NN = new NeuralNetwork();
+const NN = new NeuralNetwork(global_weights, global_biases);
 NN.init();
